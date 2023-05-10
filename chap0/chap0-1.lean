b3afcd1de9ab29cf39871903c69b53e1c8a079b7
monade <monade@gmail.com>
2023-05-10 14:52:30 +0200

    improved proofs for indiscret topology

--- chap0/chap0-1.lean
+++ chap0/chap0-1.lean
@@ -30,62 +30,77 @@ namespace Indiscret
 instance : TopologicalSpace a where
   IsOpen (x : Set a) := (x = univ) ∨ (x = ∅)
   isOpen_univ := by simp
-  isOpen_inter := by
-    intro x y hx hy
+  isOpen_inter (x : Set a) (y : Set a) hx hy := by
+    have : (x = univ) ∨ (x = ∅) := hx
+    show (x ∩ y = univ) ∨ (x ∩ y = ∅)
     cases hx with
-    | inr h =>
-        have : x ∩ y = ∅ := by simp[h]
-        apply Or.inr
-        assumption
+    | inr _ =>
+      have : x = ∅ := by assumption
+      suffices h : x ∩ y = ∅ from Or.inr h
+      have : x ∩ y = ∅ := by simp[*]
+      exact this
     | inl h =>
-        have : x ∩ y = y := by simp[h]
+        have : x = univ := h
+        show (x ∩ y = univ) ∨ (x ∩ y = ∅)
+        have : x ∩ y = y := by simp[*]
+        have : (y = univ) ∨ (y = ∅) := hy
         cases hy with
-        | inr h2 =>
-          have : x ∩ y = ∅ := by simp[h2]
-          apply Or.inr ; assumption
-        | inl h2 =>
-          have : x ∩ y = univ := by simp[h2, h]
-          apply Or.inl ; assumption
-  isOpen_unionₛ s f := byCases
-    (fun h : univ ∈ s =>
-      have : univ ⊆ ⋃₀ s := le_supₛ h
-      have : ⋃₀ s = univ := Set.eq_univ_of_univ_subset this
-      Or.inl this)
-    (fun h : ¬(univ ∈ s) => byCases
-      (fun h2 : s = ∅ =>
-        have : ⋃₀ s = ∅ := by simp[h2]
-        Or.inr this)
-      (fun _ : ¬(s = ∅) =>
-        have h3 : ∀ t, t ∈ s -> t ⊆ ∅ := fun t =>
-          fun h4 : t ∈ s =>
-            have h5 : t = univ ∨ t = ∅ := f t h4
-            Or.elim h5
-              (fun h6 : t = univ => by
-                rw[h6] at h4
-                exact absurd h4 h)
-              (fun h6 => Iff.mpr Set.subset_empty_iff h6)
-        have : ⋃₀ s = ∅ := by
-          have h4 : ∀ t, t ∈ s -> t ⊆ ∅ := fun t =>
-            fun h5 : t ∈ s => h3 t h5
-          have h5 : ⋃₀ s ⊆ ∅ := supₛ_le h4
-          exact Set.eq_empty_of_subset_empty h5
-        Or.inr this))
+        | inr _ =>
+          have : y = ∅ := by assumption
+          suffices h2 : x ∩ y = ∅ from Or.inr h2
+          have : x ∩ y = ∅ := by simp[*]
+          exact this
+        | inl _ =>
+          have : y = univ := by assumption
+          suffices h2 : x ∩ y = univ from Or.inl h2
+          have : x ∩ y = univ := by simp[this, h]
+          exact this
+  isOpen_unionₛ s f := by
+    have : Set (Set a) := s
+    have : ∀ (t : Set a), t ∈ s → t = univ ∨ t = ∅ := f
+    show (⋃₀ s = univ) ∨ (⋃₀ s = ∅)
+    exact byCases
+      (fun h : univ ∈ s => by
+        suffices h2 : ⋃₀ s = univ from Or.inl h2
+        have : univ ⊆ ⋃₀ s := le_supₛ h
+        simp[*, Iff.mp Set.univ_subset_iff])
+      (fun h : ¬(univ ∈ s) =>
+        suffices h2 : ⋃₀ s = ∅ from Or.inr h2
+        have h3 : ∀ t, t ∈ s -> t ⊆ ∅ := by
+          intro t p
+          have : Set a := t
+          have : t ∈ s := p
+          have : (t = univ) ∨ (t = ∅) := f t p
+          cases this with
+          | inl _ => simp[*] at p
+          | inr _ => simp[*]
+        have : ⋃₀ s = ∅ :=
+          have : ⋃₀ s ⊆ ∅ := supₛ_le h3
+          Set.eq_empty_of_subset_empty this
+        this)
 
 -- samples
-def n : Set Nat := univ
-example : IsOpen n := Or.inl rfl
-def s : Set Nat := setOf (fun x => x <= 3)
-example : ¬IsOpen s :=
-  (fun h : IsOpen s => Or.elim h
-    (fun x : s = univ =>
-      have h2 : ¬4 <= 3 := by simp
-      have h3 : 4 ∉ s := Iff.mpr Set.nmem_setOf_iff h2
-      have h4 : 4 ∈ s := by simp[x]
-      absurd h4 h3)
-    (fun x : s = ∅ => by
-      have h2 : 1 <= 3 := by simp
-      have h3 : 1 ∈ s := Iff.mpr Set.mem_setOf h2
-      rw[x] at h3
-      exact Iff.mp (Set.mem_empty_iff_false 1) h3))
+
+section nat_is_open
+  def nat : Set Nat := univ
+  example : IsOpen nat := Or.inl rfl
+end nat_is_open
+
+section _1_2_3_is_not_open
+  def s : Set Nat := {0, 1, 2, 3}
+  example : ¬IsOpen s := by
+    intro h
+    have : s = univ ∨ s = ∅ := h
+    cases h with
+    | inl x =>
+        have : s = univ := x
+        have : ¬ 4 ∈ s := by simp[s]
+        simp[x] at *
+    | inr x =>
+        have : s = ∅ := x
+        have : 1 ∈ s := by simp[s]
+        simp[x] at *
+
+end _1_2_3_is_not_open
 
 end Indiscret
