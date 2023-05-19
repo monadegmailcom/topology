26cf6d2a33b729cbc255d732529779ed11a5a894
monade <monade@gmail.com>
2023-05-19 21:29:43 +0200

    define a proper category of topological spaces

--- chap0/chap0-2.lean
+++ chap0/chap0-2.lean
@@ -10,14 +10,39 @@ open Topology
 open Set
 open CategoryTheory
 
-def tt := TopologicalSpace ℝ
-def tmp1 : TopCat := TopCat.of ℝ
-def tmp2 : TopCat := TopCat.of ℕ
-def f : tmp1 ⟶ tmp1 := 𝟙 tmp1
-def hom := TopCat.bundledHom
-def top : TopologicalSpace ℝ
-  := TopCat.topologicalSpaceUnbundled tmp1
+structure Topo where
+  carrier : Type u
+  top : TopologicalSpace carrier
+
+structure Mor (source : Topo) (dest : Topo) where
+  f : source.carrier -> dest.carrier
+  con : @Continuous source.carrier dest.carrier
+          source.top dest.top f
+
+instance : Category Topo where
+  Hom x y := Mor x y
+  id x := Mor.mk id
+            (@continuous_id x.carrier x.top)
+  comp := by
+    intro x y z f g
+    have fxy : Mor x y := f
+    have fyz : Mor y z := g
+    apply Mor.mk (fyz.f ∘ fxy.f)
+            (@Continuous.comp
+                x.carrier y.carrier z.carrier
+                x.top y.top z.top
+                fyz.f fxy.f
+                fyz.con fxy.con)
 
 namespace sample
+
+-- is there an easier way to define topR?
+def topR : TopologicalSpace ℝ :=
+  TopCat.topologicalSpaceUnbundled (TopCat.of ℝ)
+def top : Topo := Topo.mk ℝ topR
+def f : top ⟶ top := 𝟙 top
+def g : top ⟶ top :=
+  Mor.mk id (@continuous_id ℝ topR)
+def h : top ⟶ top := f ≫ g
 
 end sample
