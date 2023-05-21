baa01ce5d68eaca7cc5ccccdfda152fc1861a3a4
monade <monade@gmail.com>
2023-05-21 10:37:41 +0200

    some chapter/file refactoring

---
+++ chap0/chap0-2/chap0-2-1/ex0-3_rmod.lean
@@ -0,0 +1,41 @@
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.Algebra.Module.Basic
+import Mathlib.Algebra.Module.LinearMap
+
+open CategoryTheory
+
+structure Obj (R : Type u) [Semiring R] where
+  M : Type v
+  instMonoid : AddCommMonoid M
+  instModule : Module R M
+
+structure Mor [Semiring R] (s : Obj R) (d : Obj R) where
+  sd : @LinearMap _ _ _ _ (RingHom.id _) _ _
+          s.instMonoid d.instMonoid s.instModule d.instModule
+
+instance [Semiring R] : Category (Obj R) where
+  Hom := Mor
+  id x :=
+    have idLinearMap := @LinearMap.id _ _ _ x.instMonoid x.instModule
+    Mor.mk idLinearMap
+  comp := by
+    intro x y z f g
+    have xzLinearMap := @LinearMap.comp _ _ _ _ _ _
+                          _ _ _
+                          x.instMonoid y.instMonoid z.instMonoid _ _ _
+                          _ _ _
+                          _ g.sd f.sd
+
+    apply Mor.mk xzLinearMap
+
+namespace samples
+
+def module1 : Module ℕ ℕ := AddCommMonoid.natModule
+def obj1 : Obj ℕ := Obj.mk ℕ _ module1
+
+def module2 : Module ℕ ℤ := AddCommMonoid.natModule
+def obj2 : Obj ℕ := Obj.mk ℤ _ module2
+
+def f : obj1 ⟶ obj1 := 𝟙 obj1
+
+end samples
--- chap0/chap0-2/chap0-2-1/ex0-3_top.lean
+++ chap0/chap0-2/chap0-2-1/ex0-3_top.lean
@@ -1,17 +1,12 @@
 import Mathlib.Topology.Basic
 import Mathlib.Topology.Category.Top.Basic
-import Mathlib.Init.Set
-import Mathlib.Data.Set.Basic
 import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Topology.Instances.Real
-import Mathlib.Algebra.Module.Basic
 
 open Topology
 open Set
 open CategoryTheory
-
-namespace topSpace
 
 structure TopSpace where
   carrier : Type u
@@ -49,44 +44,3 @@ def g : top ⟶ top :=
 def h : top ⟶ top := f ≫ g
 
 end sample
-
-end topSpace
-
-namespace module
-
-structure Obj (R : Type u) [Semiring R] where
-  M : Type v
-  instMonoid : AddCommMonoid M
-  instModule : Module R M
-
-structure Mor [Semiring R] (s : Obj R) (d : Obj R) where
-  sd : @LinearMap _ _ _ _ (RingHom.id _) _ _
-          s.instMonoid d.instMonoid s.instModule d.instModule
-
-instance [Semiring R] : Category (Obj R) where
-  Hom := Mor
-  id x :=
-    have idLinearMap := @LinearMap.id _ _ _ x.instMonoid x.instModule
-    Mor.mk idLinearMap
-  comp := by
-    intro x y z f g
-    have xzLinearMap := @LinearMap.comp _ _ _ _ _ _
-                          _ _ _
-                          x.instMonoid y.instMonoid z.instMonoid _ _ _
-                          _ _ _
-                          _ g.sd f.sd
-
-    apply Mor.mk xzLinearMap
-
-namespace samples
-
-def module1 : Module ℕ ℕ := AddCommMonoid.natModule
-def obj1 : Obj ℕ := Obj.mk ℕ _ module1
-
-def module2 : Module ℕ ℤ := AddCommMonoid.natModule
-def obj2 : Obj ℕ := Obj.mk ℤ _ module2
-
-def f : obj1 ⟶ obj1 := 𝟙 obj1
-
-end samples
-end module
