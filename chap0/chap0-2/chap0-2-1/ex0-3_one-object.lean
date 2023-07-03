1b1c43aeb45539d9b1329134fd290a417b8ebd99
monade <monade@gmail.com>
2023-07-03 16:43:33 +0200

    add one-object monoid category

---
+++ chap0/chap0-2/chap0-2-1/ex0-3_one-object.lean
@@ -0,0 +1,21 @@
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.Algebra.Hom.Group
+import Mathlib.Algebra.Group.Defs
+
+open CategoryTheory
+
+-- object type with only one unique instance
+structure Obj (a : Type u)
+
+-- morphism are the instances of type a
+
+instance [Monoid a] : CategoryStruct (Obj a) where
+  Hom _ _ := a
+  id _ := One.one
+  comp := HMul.hMul
+--  comp f g := f * g
+
+instance [Monoid a] : Category (Obj a) where
+  assoc := Semigroup.mul_assoc
+  id_comp := Monoid.one_mul
+  comp_id := Monoid.mul_one
